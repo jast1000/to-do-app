d38b2514e728a0b4898a3949e992eb57da81fb6b
jesussanchez@motumweb.com <GI050893?>
2019-08-07 10:19:13 -0500

    Actualizacion 2

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -12,5 +12,30 @@ export class NotesService {
   constructor(
     private http: HttpClient
   ) { }
-  
+
+  getAll(userId: string) {
+    const url = `${ this.baseUrl }/users/${ userId }/notes`;
+    return this.http.get(url).toPromise();   
+  }
+
+  find(userId: string, noteId: number) {
+    const url = `${ this.baseUrl }/users/${ userId }/notes/${ noteId }`;
+    return this.http.get(url).toPromise();
+  }
+
+  save(userId: string, note: any) {
+    const url = `${ this.baseUrl }/users/${ userId }/notes`;
+    return this.http.post(url, note).toPromise();
+  }
+
+  update(userId: string, noteId: number, note: any) {
+    const url = `${ this.baseUrl }/users/${ userId }/notes/${ noteId }`;
+    return this.http.put(url, note).toPromise();
+  }
+
+  delete(userId: string, noteId: number) {
+    const url = `${ this.baseUrl }/users/${ userId }/notes/${ noteId }`;
+    return this.http.delete(url).toPromise();
+  }
+
 }
